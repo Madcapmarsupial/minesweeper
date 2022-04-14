db650532dc74e5cebeecc8ef27bb3e503167f139
Numbat <samueltylergreen@gmail.com>
2022-04-13 18:23:38 -0700

    add find neighor tiles function

--- lib/board.rb
+++ lib/board.rb
@@ -7,16 +7,9 @@ class Board
         grid = Array.new(height) { Array.new(width) }
     end
 
-    def [](index)
-        @grid[index]
-    end
-   
-    def []=(index, value)
-        @grid[index] = value
-    end
-
     def initialize(level)
         difficulties = { 
+            "test" => [3,3, 2],
             "beginner" => [9, 9, 10], 
             "intermediate" => [16, 16, 10],
             "expert" => [16, 30, 99] 
@@ -26,12 +19,22 @@ class Board
         @width = difficulty[1]
         @mine_count = difficulty.last
         @grid = Board.make_empty_grid(height, width)
+        self.fill_grid
+        p self.seed_mines
+    end
+    
+    def [](index)
+        @grid[index]
+    end
+   
+    def []=(index, value)
+        @grid[index] = value
     end
 
     def fill_grid
-        @grid.map! do |row|
-            row.map! do |ele|
-                ele = Tile.new(self)
+        @grid.each_with_index do |row, i|
+            row.each_with_index do |ele, j|
+                @grid[i][j]= Tile.new(self, [i, j])
             end
         end
         "done"
@@ -39,21 +42,28 @@ class Board
 
     def seed_mines
         coords = []
-
-        (0...mine_count).each do |i|
-            h = rand(height - 1)
-            w = rand(width - 1)
+        #(0...mine_count).each do |i|
+        mines_placed = 0
+        until @mine_count == mines_placed
+            h = rand(height)
+            w = rand(width)
 
             if coords.include?([h, w])
-                i -= 1
+                next
             else 
-                coords << [h, w] 
                 @grid[h][w].set_mine!
+                coords << [h, w] 
+                mines_placed += 1
             end
         end
         p coords
         "mines set!"
     end
 
-    
+end
+
+if  __FILE__ == $PROGRAM_NAME
+     b = Board.new("beginner")
+     b.fill_grid
+     b.seed_mines
 end
--- lib/tile.rb
+++ lib/tile.rb
@@ -1,39 +1,66 @@
 #require 'colorize'
 
 class Tile
-    attr_accessor :value, :bombed, :flagged, :revealed
+    attr_accessor :value, :bombed, :flagged, :revealed, :pos
     attr_reader :board
 
-    def initialize(board)
+    def initialize(board, pos)
         @value = "*"
         @board = board
+        @pos = pos
         @bombed = false
         @flagged = false
         @revealed = false
-        #@neighbors_List
     end
 
-    def neighbors
+    def get_list
+        y, x = @pos
+        neighbors_list = []
+        (y-1..y+1).each do |h|  
+            (x-1..x+1).each do |w| 
+                if w.between?(0, @board.width-1) && h.between?(0, @board.height-1)
+                    neighbors_list << [h, w]  
+                end
+            end 
+        end
+        neighbors_list.delete(@pos)
+        return neighbors_list
+    end
 
+    def neighbor_tiles(list_of_neighbors)
+        list_of_neighbors.map! do |coords|
+            h, w = coords
+            @board[h][w]
+        end
+    end
+
+    
+    def reveal 
+       @value = self.neighbor_bomb_count
     end
 
     def neighbor_bomb_count
-    end
+        return "X" if bombed
 
-    def reveal 
+        list = self.get_list
+        neighbors = neighbor_tiles(list)
+        bomb_count = 0
+        neighbors.each { |neighbor| bomb_count += 1 if neighbor.bombed }
+
+        if bomb_count == 0 
+            return  "_"
+        else 
+            p bomb_count
+            return bomb_count.to_s
+        end
+    end 
+
+    def inspect
+        @value
     end
 
     def set_mine!
         @bombed = true
     end
 
-    def inspect
-        @value 
-            #'bombed' => @bombed, 'flagged' => @flagged, 'revealed' => @revealed }
-    end 
-
-    def values
-        {'bombed' => @bombed, 'flagged' => @flagged, 'revealed' => @revealed }
-
-    end
 end
